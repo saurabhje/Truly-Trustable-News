148de3fd1abef220578bba45ae89a715450278c6
saurabhje <work.saurabh@outlook.com>
2023-07-18 07:17:51 +0530

    Building the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './component/header'
+import Footer from './component/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="header">
+        <Header/>
+      </div>
+      <div className="content"></div>
+      <div className="footer">
+        <Footer/>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
