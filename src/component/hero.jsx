c5f007e7ffc5c1144734986f4564c6c07ab250f2
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-07-19 15:18:52 +0530

    responsiveness

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <h1>This is the story of the century and we are crazy about it also what the 
                 hell are you talking about you crazy bastard.
             </h1>
-            <h1> Author Name</h1>
+            <h2> Author Name</h2>
         </div>
     </div>
   )
