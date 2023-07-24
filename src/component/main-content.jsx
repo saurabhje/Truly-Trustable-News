d205638203f11e245e46b3ea3cf74ada12b79676
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-07-24 18:27:37 +0530

    mobile friendly content

--- src/component/main-content.jsx
+++ src/component/main-content.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "/main-content.css"
 
 const MainContent = () => {
-  const [threelayout, setthreelayout] = useState(true)
-  const Changelayout = () =>{
-    if(threelayout === true){
-      setthreelayout(false);
-    }
-    else{
-      setthreelayout(true);
+  const [mobileview, setmovileview] = useState(false)
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setmovileview(true)
+    } 
+    else {
+      setmovileview(false)
     }
   }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+  })
+  
   return (
     <>
-      <div style = {threelayout ? {"grid-template-columns": "1fr 1fr 1fr"} : {"grid-template-columns":"1fr 1fr", "gap": "60px"}} className='content'>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
-        <div style = {threelayout ? {"height": "30vh"} : {"height": "40vh"}} className="item"></div>
+      <div style= {mobileview ? {"grid-template-columns": "1fr"}: {"grid-template-columns": "1fr 1fr"}} className='content'>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
       </div>
-      <div className='sidebar'>
-        <button onClick={Changelayout}>
-          <h1 style = {{"color": "white"}}>CHANGE LAYOUT</h1>
-        </button>
-      </div>
+      {mobileview || 
+        <div className='sidebar'>
+        </div>}
     </>
   )
 }
