230927916e5fee36497011fed1b51169fe533cf1
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-07-24 18:08:04 +0530

    added a change layout button temp

--- src/component/main-content.jsx
+++ src/component/main-content.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import "/main-content.css"
 
 const MainContent = () => {
+  const [threelayout, setthreelayout] = useState(true)
+  const Changelayout = () =>{
+    if(threelayout === true){
+      setthreelayout(false);
+    }
+    else{
+      setthreelayout(true);
+    }
+  }
   return (
     <>
-      <div className='content'>
+      <div style = {threelayout ? {"grid-template-columns": "1fr 1fr 1fr"} : {"grid-template-columns":"1fr 1fr", "gap": "60px"}} className='content'>
         <div className="item"></div>
         <div className="item"></div>
         <div className="item"></div>
@@ -16,7 +26,11 @@ const MainContent = () => {
         <div className="item"></div>
         <div className="item"></div>
       </div>
-      <div className='sidebar'></div>
+      <div className='sidebar'>
+        <button onClick={Changelayout}>
+          <h1 style = {{"color": "white"}}>CHANGE LAYOUT</h1>
+        </button>
+      </div>
     </>
   )
 }
