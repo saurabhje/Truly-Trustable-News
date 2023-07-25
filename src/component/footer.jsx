cb035b04ea0d670de91c30fa8336c15765278c76
saurabhje <work.saurabh@outlook.com>
2023-07-25 23:36:07 +0530

    Fixed the footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
-const Footer = () => {
 
- const FooterWrapper = styled.div`
-    background-color:darkslateblue;
-    `;
+const Footer = () => {
+  const FooterWrapper = styled.div`
+    background-color: grey;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2vh 2vw;
+  `;
+
+  const Heading = styled.h1`
+    font-size: 3em;
+    margin: 0;
+  `;
+
+  const Socials = styled.div`
+    display: flex;
+    gap: 10px;
+  `;
+
+  const Icon = styled.i`
+    color: black;
+    font-size: 3em;
+  `;
+
+  const RightsReserved = styled.p`
+    font-size: 1.3em;
+    margin: 10px 0 0 0;
+  `;
+
   return (
-    <FooterWrapper> footer </FooterWrapper>
-  )
-}
+    <FooterWrapper>
+      <Heading>The Humanist</Heading>
+      <Socials>
+        <a href='#'>
+          <Icon className='fab fa-instagram'></Icon>
+        </a>
+        <a href='#'>
+          <Icon className='fab fa-twitter'></Icon>
+        </a>
+      </Socials>
+      <RightsReserved>All rights reserved to JeX</RightsReserved>
+    </FooterWrapper>
+  );
+};
+
 export default Footer;
