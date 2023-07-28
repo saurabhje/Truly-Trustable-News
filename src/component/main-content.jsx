3f20fbf29a4a443b5a2d5b4263a6fdf8251f62e8
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-07-28 17:06:42 +0530

    placeholder content styling

--- src/component/main-content.jsx
+++ src/component/main-content.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import "/main-content.css"
+import "./main-content.css"
 
 const MainContent = () => {
   const [mobileview, setmovileview] = useState(false)
@@ -19,19 +19,104 @@ const MainContent = () => {
   return (
     <>
       <div style= {mobileview ? {"grid-template-columns": "1fr"}: {"grid-template-columns": "1fr 1fr"}} className='content'>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap1'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
+        <div className="item">
+          <div className='itemwrap2'>
+            <img id="img" src='./src/assets/poco.jpg'></img>
+            <h1>this is the heading</h1>
+          </div>
+          <p>bla bla bla bla bla bla bla lb narendra modi bla lbal dlad hmklajmkclamrk nlknakll mfdsklm fkdsmnf kldjf kldsngjdf jgkldn kgj ksngkdfm gfdkng kdfmgdklm gkldfg kldfjgklrf kgldfnvkl dfkglv kdfog kdfklgjm kdflgm  </p>
+        </div>
       </div>
       {mobileview || 
         <div className='sidebar'>
