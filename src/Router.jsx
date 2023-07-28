20ccf5e4c010c13c83d685bbb1f2f9e0e02a2c4f
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-07-28 23:32:35 +0530

    added dynamic hero

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,16 +9,19 @@ import Newspage from './component/newspage'
 
 
 function Router() {
+  const[currenttitle, setCurrenttitle] = useState("This is the story of the century and we are crazy about it also what the hell are you talking about you crazy bastard.")
+  const[currentimg, setCurrentimg] = useState("../src/assets/poco.jpg")
+  const[currentauth, setCurrentauth] = useState("Author Name")
 
   return (
     <BrowserRouter>
-      <div className="header">
+      <div style={{backgroundImage: `url(${currentimg})`}} className="header">
         <Navbar/>
-        <Hero />
+        <Hero currenttitle={currenttitle} currentauth={currentauth}/>
       </div>  
         <Routes>
           <Route path="/" element={<MainContent/>}/>
-          <Route path="/news1" element={<Newspage/>}/>
+          <Route path="/news1" element={<Newspage setCurrenttitle={setCurrenttitle} setCurrentimg={setCurrentimg} setCurrentauth={setCurrentauth}/>}/>
         </Routes>
         <div className="footer">
         <Footer/>
