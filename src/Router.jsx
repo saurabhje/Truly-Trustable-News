b1b14e7a810cd270d05ed442e773b148c2963472
saurabhje <work.saurabh@outlook.com>
2023-07-29 00:41:51 +0530

    Newspage

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,31 +1,15 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import './Router.css'
-import Navbar from './component/navbar'
-import Hero from './component/hero'
-import Footer from './component/footer'
-import MainContent from './component/main-content'
+import './Router.css';
+import Homepage from './component/homepage';
 import Newspage from './component/newspage'
 
-
 function Router() {
-  const[currenttitle, setCurrenttitle] = useState("This is the story of the century and we are crazy about it also what the hell are you talking about you crazy bastard.")
-  const[currentimg, setCurrentimg] = useState("../src/assets/poco.jpg")
-  const[currentauth, setCurrentauth] = useState("Author Name")
-
   return (
-    <BrowserRouter>
-      <div style={{backgroundImage: `url(${currentimg})`}} className="header">
-        <Navbar/>
-        <Hero currenttitle={currenttitle} currentauth={currentauth}/>
-      </div>  
+    <BrowserRouter>  
         <Routes>
-          <Route path="/" element={<MainContent/>}/>
-          <Route path="/news1" element={<Newspage setCurrenttitle={setCurrenttitle} setCurrentimg={setCurrentimg} setCurrentauth={setCurrentauth}/>}/>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/news1" element={<Newspage />}/>
         </Routes>
-        <div className="footer">
-        <Footer/>
-      </div>
     </BrowserRouter>
   )
 }
---
+++ src/component/homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from './hero'
+import Navbar from './navbar'
+import Footer from './footer'
+import MainContent from './main-content'
+import poco from "../assets/poco.jpg";
+const Homepage = () => {
+  return (
+    <div>
+        <div className="header"  style={{ backgroundImage: `url(${poco})` }}>
+            <Navbar/>
+            <Hero/>
+        </div>
+        <div className="main-content">
+            <MainContent/>
+        </div>
+        <div className="footer">
+            <Footer/>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage;
