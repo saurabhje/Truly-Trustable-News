f147b2ee3b3319ca3cc09cce0427fe5e99b29d87
saurabhje <saurabhjesingh@gmail.com>
2023-10-05 00:41:53 +0530

    used tailwaing cause got bored

--- Frontend/src/component/create-news.jsx
+++ Frontend/src/component/create-news.jsx
@@ -2,16 +2,24 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 function Create_news() {
-  const [name, setName] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    article: ""
+  });
 
   const handleChange = (event) => {
-    setName(event.target.value);
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   }
 
   const handleSubmit = (event) => {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
 
-    axios.post('http://localhost:3000/create-news', { name })
+    axios.post('http://localhost:3000/create-news', formData)
       .then((response) => {
         console.log(response.data.message);
       })
@@ -21,13 +29,35 @@ function Create_news() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} name="name" onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new News Article</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title : </label>
+              <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Title of the news" required onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author : </label>
+              <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <option selected="">Select category</option>
+                <option value="TV">TV/Monitors</option>
+                <option value="PC">PC</option>
+              </select>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="article" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Article : </label>
+              <textarea id="article" name="article" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="News article" onChange={handleChange} required></textarea>
+            </div>
+          </div>
+          <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+            Add Article
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
 
