a345c11eeb00794ab749a59915dbca3399fa92f9
saurabhje <saurabhjesingh@gmail.com>
2023-10-04 18:46:52 +0530

    main is fucked

--- Frontend/src/component/login.jsx
+++ Frontend/src/component/login.jsx
@@ -32,6 +32,7 @@ const Login = () =>{
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
     return(
+        // why are you doing this? this would be better if we did this in the backend part
         <>
             {loggedin?
             <form onSubmit={handleSubmit}>
