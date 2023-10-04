ac7afefbc3c5f0eff16064e7966efc1ed9981032
saurabhje <saurabhjesingh@gmail.com>
2023-10-05 00:47:31 +0530

    successfully sending data

--- Frontend/src/component/create-news.jsx
+++ Frontend/src/component/create-news.jsx
@@ -40,11 +40,17 @@ function Create_news() {
             </div>
             <div>
               <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author : </label>
-              <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                <option selected="">Select category</option>
+              <select
+                id="category"
+                name="author"
+                value={formData.author}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option value="">Select an author</option>
                 <option value="TV">TV/Monitors</option>
                 <option value="PC">PC</option>
-              </select>
+            </select>
             </div>
 
             <div className="sm:col-span-2">
