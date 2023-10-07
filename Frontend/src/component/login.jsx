481e6bde028094c431d512c55078e0fa8d81a1da
saurabhje <saurabhjesingh@gmail.com>
2023-10-07 12:49:26 +0530

    did nothing tbh

--- Frontend/src/component/login.jsx
+++ Frontend/src/component/login.jsx
@@ -1,53 +1,61 @@
-import React from 'react'
-import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AdminPage from './CRUD/AdminPage';
 
-const Login = () =>{
-    const [loggedin, setLoggedin] = useState(true);
-    const [formData, setFormData] = useState({ password: '' });
-    const navigate = useNavigate();    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-            });
-            document.getElementById("password").value = "";
-            if(response.ok){
-                setLoggedin(false)
-            }
-            else{
-                navigate('/')
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+const Login = () => {
+  const [loggedin, setLoggedin] = useState(true);
+  const [formData, setFormData] = useState({ password: '' });
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-    return(
-        <>
-            {loggedin?
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Password:
-                    <input id="password" type="pasword" name="password" value={formData.password}
-            onChange={handleChange}/>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>:
-            <AdminPage/>
-            }       
-        </>
-        
-    )
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        });
+        document.getElementById("password").value = "";
+        if(response.ok){
+            setLoggedin(false)
+        }
+        else{
+            navigate('/')
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      {loggedin ?
+        <form onSubmit={handleSubmit} className="text-center flex items-center justify-center h-screen">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            value={formData.password}
+            placeholder="Password"
+            className='mx-5 py-2 px-4'
+          />
+          <button
+            type="submit"
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mx-5 py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Login
+          </button>
+        </form>:
+        <AdminPage />
+    }
+    </>
+  );
+};
 
 export default Login;
