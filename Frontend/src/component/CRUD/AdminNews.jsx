16708010f83574e4ee5453616cda5ea82bb82c7e
saurabhje <saurabhjesingh@gmail.com>
2023-10-23 22:24:44 +0530

    delete pop up

--- Frontend/src/component/CRUD/AdminNews.jsx
+++ Frontend/src/component/CRUD/AdminNews.jsx
@@ -5,6 +5,8 @@ import './AdminNews.css'
 const AdminNews = () => {
     const [headings, setHeadings] = useState([]);
     const [deletionError,setDeletionError] = useState('');
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [newsItemIdToDelete, setNewsItemIdToDelete] = useState(null);
 
     useEffect(() => {
         fetchData();
@@ -18,11 +20,13 @@ const AdminNews = () => {
           console.error('Error fetching data:', error);
         }
       };
-    
+      
+
       const deleteNews = async (id) => {
         setHeadings((prevHeadings) => prevHeadings.filter(e => e._id !== id));
         setDeletionError(null);
-    
+        setShowPopUp(false);
+
         try {
           const response = await axios.post(`http://localhost:3000/delete/${id}`);
           console.log(response);
@@ -31,6 +35,11 @@ const AdminNews = () => {
           setDeletionError('Error deleting the news item. Please try again.');
         }
       };
+
+      function closePopUp(){
+        setShowPopUp(false);
+        setNewsItemIdToDelete(null);
+      }
 
   return (
     <div>
@@ -41,10 +50,26 @@ const AdminNews = () => {
             <div className='newsbox bg-slate-300 rounded px-2 py-5 md:px-5'  key={e._id}>
                 <h3 className='newsheading font-normal mt-0'>Title: {e.heading}</h3>
                 <button className="py-2 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 border-none rounded focus:shadow-outline hover:bg-indigo-800">Edit</button>             
-                <button className="py-2 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 border-none rounded focus:shadow-outline hover:bg-red-800">Delete</button>
+                <button
+                        className="py-2 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 border-none rounded focus:shadow-outline hover:bg-red-800"
+                        onClick={() => {
+                          setNewsItemIdToDelete(e._id);
+                          setShowPopUp(true);
+                        }}>
+                        Delete
+              </button>            
             </div>
         ))}
       </div>
+      {showPopUp && (
+        <div className="popup">
+          <div className="popup-content ">
+            <p>Are you sure you want to delete this item?</p>
+            <button onClick={closePopUp}>Cancel</button>
+            <button className="py-2 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 border-none rounded focus:shadow-outline hover:bg-red-800" onClick={() => deleteNews(newsItemIdToDelete)}>Delete</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
