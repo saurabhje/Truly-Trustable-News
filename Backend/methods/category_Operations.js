5dccccdae7a3e742e4306bdd2cc90141d473d73d
saurabhje <saurabhjesingh@gmail.com>
2023-11-02 21:43:35 +0530

    m

--- Backend/methods/category_Operations.js
+++ Backend/methods/category_Operations.js
@@ -3,4 +3,15 @@ const Category = require('../model/categoryschema');
 
 const asyncHandler = require("express-async-handler");
 
+exports.category_news_list = asyncHandler( async (req,res,next)=>{
+    const [ category, categoryNews ] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        News.find({ category: req.params.id },"title,author,").exec()
+    ]);
+    res.json(category,categoryNews);
+} )
 
+exports.allCategories = asyncHandler( async(req, res, next ) => {
+    const allCategories = await Category.find().sort({ date: -1 });
+    res.json(allCategories);
+})
