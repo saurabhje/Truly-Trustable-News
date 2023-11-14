7285ed6ab71b881d3cfddab3766320db8c3e121b
saurabhje <saurabhjesingh@gmail.com>
2023-11-14 16:46:42 +0530

    CreateNews Styling

--- Frontend/src/component/CRUD/CreateNews.jsx
+++ Frontend/src/component/CRUD/CreateNews.jsx
@@ -20,34 +20,39 @@ function CreateNews() {
   };
 
   const [formData, setFormData] = useState(initialFormData);
-  const [ allcategories, setallCategories ] = useState([]);
+  const [allcategories, setallCategories] = useState([]);
   const [loggedin, setLoggedin] = useState(false);
 
-  useEffect(()=>{
-    if(sessionStorage.getItem("password")){
+  useEffect(() => {
+    if (sessionStorage.getItem("password")) {
       authorise();
     }
-  }, [])
+  }, []);
 
   const authorise = async () => {
-    axios.post('http://localhost:3000/login', {
-      password: sessionStorage.getItem("password"),
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-   })
-    .then((response) => {
-      if (response.status === 200) {
-        setLoggedin(true);
-      } else{
-        navigate('/login');
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  }
+    axios
+      .post(
+        "http://localhost:3000/login",
+        {
+          password: sessionStorage.getItem("password"),
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          setLoggedin(true);
+        } else {
+          navigate("/login");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   async function getData(id) {
     try {
       const res = await axios.get(`http://localhost:3000/edit/${id}`);
@@ -66,13 +71,13 @@ function CreateNews() {
     }
   }
 
-  async function getCats(){
-    try 
-      { const res = await axios.get('http://localhost:3000/categories')
-        console.log(res)
-        setallCategories(res.data);
-    } catch(error) {
-      console.log('Error fetching categories')
+  async function getCats() {
+    try {
+      const res = await axios.get("http://localhost:3000/categories");
+      console.log(res);
+      setallCategories(res.data);
+    } catch (error) {
+      console.log("Error fetching categories");
     }
   }
   useEffect(() => {
@@ -80,7 +85,7 @@ function CreateNews() {
     if (id !== undefined) {
       getData(id);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const handleChange = (event) => {
@@ -137,144 +142,120 @@ function CreateNews() {
         // Handle errors
       });
   };
-  return (
-    loggedin?
-      <div className="flex flex-col h-screen">
-        <h2 className="font-normal text-center text-xl md:text-4xl">
-          Edit News
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="title"
-                className="block mb-2 text-base font-medium text-gray-900"
-              >
-                Title:
-              </label>
-              <input
-                type="text"
-                name="heading"
-                id="title"
-                className="md:text-base bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="Title of the news"
-                required
-                value={formData.heading}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="subhead"
-                className="text-base block mb-2 text-sm font-medium text-gray-900"
-              >
-                Subheading:
-              </label>
-              <input
-                type="text"
-                name="subheading"
-                id="subhead"
-                className="md:text-base bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="Subheading"
-                required
-                value={formData.subheading}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="img_url">Img:</label>
-              <input
-                type="text"
-                name="img_url"
-                id="img_url"
-                placeholder="Image URL"
-                required
-                value={formData.img_url}
-                onChange={handleChange}
-              />
-            </div>
-            <div style={{ "marginLeft": "-28.5vw", "height": "55vh", "width": "100vw", "backgroundImage": `url(${formData.img_url})`, "backgroundSize": "cover", "backgroundPosition": formData.img_pos }}>
-            </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="img_pos">Position:</label>
-              <input
-                type="text"
-                name="img_pos"
-                id="img_pos"
-                placeholder="Image Position"
-                value={formData.img_pos}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="author"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Author:
-              </label>
-              <select
-                id="author"
-                name="author"
-                value={formData.author}
-                onChange={handleChange}
-              >
-                <option value="">Select an author</option>
-                <option value="Je">Je</option>
-                <option value="XuNigger">XuNigger</option>
-              </select>
-            </div>
-            <div>
-              <label className="block mb-2 text-base font-medium text-gray-900">
-                Category:
-              </label>
-              <div>
-                  <label className="block mb-2 text-base font-medium text-gray-900">
-                    Category:
-                  </label>
-                  {allcategories.map((category) => (
-                    <label key={category._id} className="block text-sm font-medium text-gray-900">
-                      <input
-                        type="checkbox"
-                        name="category"
-                        value={category._id}
-                        checked={formData.category.includes(category._id)}
-                        onChange={handleCategoryChange}
-                      />
-                      {category.title}
-                    </label>
-                  ))}
-                  </div>
-            </div>
-
-            <div className="sm:col-span-2 h-5/6">
-              <label
-                htmlFor="article"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Article:
-              </label>
-              <ReactQuill
-                name="article"
-                className="md:text-xl"
-                id="article"
-                theme="snow"
-                value={formData.article}
-                onChange={handleChangeQuill} 
-              />
-            </div>
+  return loggedin ? (
+    <div>
+      <h2 className="font-normal text-center text-xl md:text-4xl">Edit News</h2>
+      <form className="newsform" onSubmit={handleSubmit}>
+        <div className="formData">
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              name="heading"
+              id="title"
+              placeholder="Title of the news"
+              required
+              value={formData.heading}
+              onChange={handleChange}
+            />
           </div>
-          <button
-            type="submit"
-            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-black bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover.bg-primary-800"
-          >
-            Save Changes
-          </button>
-        </form>
-      </div>
-    : <div>
+          <div>
+            <label htmlFor="subhead">Subheading:</label>
+            <input
+              type="text"
+              name="subheading"
+              id="subhead"
+              placeholder="Subheading"
+              required
+              value={formData.subheading}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="img_url">Img:</label>
+            <input
+              type="text"
+              name="img_url"
+              id="img_url"
+              placeholder="Image URL"
+              required
+              value={formData.img_url}
+              onChange={handleChange}
+            />
+          </div>
+          <div
+            style={{
+              height: "55vh",
+              width: "100vw",
+              backgroundImage: `url(${formData.img_url})`,
+              backgroundSize: "cover",
+              backgroundPosition: formData.img_pos,
+            }}
+          ></div>
+          <div>
+            <label htmlFor="img_pos">Position:</label>
+            <input
+              type="text"
+              name="img_pos"
+              id="img_pos"
+              placeholder="Image Position"
+              value={formData.img_pos}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="author">Author:</label>
+            <select
+              id="author"
+              name="author"
+              value={formData.author}
+              onChange={handleChange}
+            >
+              <option value="">Select an author</option>
+              <option value="Je">Je</option>
+              <option value="XuNigger">XuNigger</option>
+            </select>
+          </div>
+          <div>
+            <label>Category:</label>
+            {allcategories.map((category) => (
+              <label key={category._id}>
+                <input
+                  type="checkbox"
+                  name="category"
+                  value={category._id}
+                  checked={formData.category.includes(category._id)}
+                  onChange={handleCategoryChange}
+                />
+                {category.title}
+              </label>
+            ))}
+          </div>
+
+          <div>
+            <label
+              htmlFor="article"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Article:
+            </label>
+            <ReactQuill
+              name="article"
+              id="article"
+              theme="snow"
+              value={formData.article}
+              onChange={handleChangeQuill}
+            />
+          </div>
+        </div>
+        <button type="submit">Save Changes</button>
+      </form>
+    </div>
+  ) : (
+    <div>
       <p>
-        Not Logged in, Login Rn <button onClick={()=> navigate('/reallybreh12')}>Login</button>
+        Not Logged in, Login Rn{" "}
+        <button onClick={() => navigate("/reallybreh12")}>Login</button>
       </p>
     </div>
   );
