a4a7693afd8e5fbdcae165e9c1564a79f4efc7a3
saurabhje <saurabhjesingh@gmail.com>
2023-11-16 00:47:09 +0530

    Organizing routes and restructured dir

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,17 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const mongoose = require('mongoose');
-const news_operations = require('./src/methods/news_operations');
-const category_Operations = require('./src/methods/category_Operations');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const routes = require("./src/routes");
 
 mongoose.set("strictQuery", false);
 
 const mongoDB = process.env.MONGODB_URI;
+main().catch((err)=> console.log(err));
+async function main(){
+  await mongoose.connect(mongoDB);
+}
 
 app.use( bodyParser.json() );  
 app.use(bodyParser.urlencoded({    
@@ -19,33 +22,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.use(express.json());   
 app.use(express.urlencoded());
-
-main().catch((err)=> console.log(err));
-async function main(){
-  await mongoose.connect(mongoDB);
-}
-
-
-
-app.get('/admin', news_operations.allNews);
-app.get('/', news_operations.allNewsHome);
-app.get('/news/:id', news_operations.getNews);
-app.post('/create-news',news_operations.create_News_post);
-app.post('/delete/:id',news_operations.delete_news);
-app.get('/edit/:id', news_operations.edit_news_get);
-app.post('/edit/:id', news_operations.edit_News_post);
-
-app.get('/categories',category_Operations.allCategories);
-app.get('/category/:id', category_Operations.category_news_list);
-
-app.post('/login', (req, res, next) => {
-  if(req.body.password === process.env.PASSWORD){
-    res.status(200).json({ message: 'Login successful' });
-  } else {
-    console.log(req.body.password)
-    res.status(401).json({ message: 'Login failed: Incorrect password' });
-  }
-})
+app.use("/",routes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
