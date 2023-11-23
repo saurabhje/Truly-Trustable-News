497c0fee89ad4a341941b18b22966e62064afc07
saurabhje <saurabhjesingh@gmail.com>
2023-11-23 22:29:13 +0530

    Hamburger

--- Frontend/src/component/navbar.jsx
+++ Frontend/src/component/navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './navbar.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-      <Link to="/">The Third Front</Link>
+        <Link to="/">The Third Front</Link>
       </div>
+      <button className='hamburger fa-solid fa-bars'>
+      </button>
       <div className='nav-link'>
-        <Link to="/aboutus">BOUT</Link>
-        <Link to="/contactus">CONTACK</Link>
+        <Link to="/aboutus">ABOUT</Link>
+        <Link to="/contactus">CONTACT</Link>
         <button className='support-btn'>SUPPORT US</button>
       </div>
     </div>
