508b8d0d3ef618a377840fa37026d2183e95b11f
saurabhje <saurabhjesingh@gmail.com>
2023-11-27 03:25:35 +0530

    Font Family

--- Frontend/src/component/footer.jsx
+++ Frontend/src/component/footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   `;
 
   const Heading = styled.h1`
-    font-family: 'Inter';
+  font-family: 'Times New Roman', Times, serif;
     font-size: 2em;
     margin: 0;
     @media (max-width: 400px){
