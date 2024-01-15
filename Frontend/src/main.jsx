ace8e4a258b4c193f372ab118427fc4de168687b
saurabhje <work.surabh@outlook.com>
2024-01-16 00:02:08 +0530

    Fix font import delay

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './component/Router.jsx'
 import './index.css'
-
+import store from './component/store.js'
+import { Provider } from 'react-redux'
+import "@fontsource/anton"
+import '@fontsource/oswald'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router />
+    <Provider store={store}>
+        <Router />
+    </Provider>
 )
