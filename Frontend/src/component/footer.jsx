df6053cfe954aa46d3afb12e19c1cda785f132fe
0xamitr <0xamitr@gmail.com>
2024-01-22 22:54:41 +0530

    fix suppress styled components warning

--- Frontend/src/component/footer.jsx
+++ Frontend/src/component/footer.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 
-const Footer = () => {
-  const FooterWrapper = styled.div`
-    border-top: 1px solid rgba(255, 255, 255, 0.5);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 1% 2.5%;
-    width: 95%;
-  `;
+const FooterWrapper = styled.div`
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1% 2.5%;
+  width: 95%;
+`;
 
-  const Heading = styled.h1`
-    font-family: "Times New Roman", Times, Baskerville, Georgia, serif;
-    font-size: 2em;
-    margin: 0;
-    @media (max-width: 400px) {
-      font-family: "Times New Roman";
-      font-size: 1em;
-    }
-  `;
-
-  const RightsReserved = styled.p`
-    font-family: Inter;
+const Heading = styled.h1`
+  font-family: "Times New Roman", Times, Baskerville, Georgia, serif;
+  font-size: 2em;
+  margin: 0;
+  @media (max-width: 400px) {
+    font-family: "Times New Roman";
     font-size: 1em;
-    margin: 10px 0 0 0;
-    @media (max-width: 600px) {
-      font-size: 0.7em;
-    }
-  `;
+  }
+`;
+
+const RightsReserved = styled.p`
+  font-family: Inter;
+  font-size: 1em;
+  margin: 10px 0 0 0;
+  @media (max-width: 600px) {
+    font-size: 0.7em;
+  }
+`;
+
+const Footer = () => {
 
   return (
     <FooterWrapper>
